9a79dfef67e05ddb5d0ce221b5d95944f9a6b013
Fedor <dfdobry@gmail.com>
2022-03-25 17:56:55 +0300

    Добавил в конвертер массу, для примера С 10 заданием посчитал массив

--- src/TaskSix.java
+++ src/TaskSix.java
@@ -7,59 +7,92 @@ public class TaskSix {
         int operation = console.nextInt();
         switch (operation) {
             case 1:
-                System.out.println("Конвертер не считает массу");
-                break;
-            case 2:
-                System.out.println("Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут");
-                int value = console.nextInt();
-                double meters;
-                double miles;
-                double yards;
-                double futs;
+                System.out.println("Выберите единицу измерения: 1 - киллограмм, 2 - грамм, 3 - тонна");
+                int value2 = console.nextInt();
+                double kg;
+                double gramm;
+                double ton;
                 System.out.println("Введите число:");
-                int count = console.nextInt();
-                switch (value) {
+                int count2 = console.nextInt();
+                switch (value2) {
                     case 1:
-                        meters = count * 1;
-                        miles = count / 1609.34;
-                        yards = count * 1.094;
-                        futs = count * 3.281;
-                        System.out.println("Метры " + meters);
-                        System.out.println("Мили " + miles);
-                        System.out.println("Ярды " + yards);
-                        System.out.println("Футы " + futs);
+                        kg = count2 * 1;
+                        gramm = count2 * 1000;
+                        ton = count2 / 1000;
+                        System.out.println("киллограммы " + kg);
+                        System.out.println("граммы " + gramm);
+                        System.out.println("тонна " + ton);
                         break;
                     case 2:
-                        meters = count * 1609;
-                        miles = count * 1;
-                        yards = count * 1760;
-                        futs = count * 5280;
-                        System.out.println("Метры " + meters);
-                        System.out.println("Мили " + miles);
-                        System.out.println("Ярды " + yards);
-                        System.out.println("Футы " + futs);
+                        kg = count2 / 1000;
+                        gramm = count2 * 1;
+                        ton = count2 / 1e-6;
+                        System.out.println("киллограммы " + kg);
+                        System.out.println("граммы " + gramm);
+                        System.out.println("тонна " + ton);
                         break;
                     case 3:
-                        meters = count / 1.094;
-                        miles = count / 1760;
-                        yards = count * 1;
-                        futs = count * 3;
-                        System.out.println("Метры " + meters);
-                        System.out.println("Мили " + miles);
-                        System.out.println("Ярды " + yards);
-                        System.out.println("Футы " + futs);
+                        kg = count2 * 1000;
+                        gramm = count2 * 1e+6;
+                        ton = count2 * 1;
+                        System.out.println("киллограммы " + kg);
+                        System.out.println("граммы " + gramm);
+                        System.out.println("тонна " + ton);
                         break;
-                    case 4:
-                        meters = count / 3.281;
-                        miles = count / 5280;
-                        yards = count / 3;
-                        futs = count * 1;
-                        System.out.println("Метры " + meters);
-                        System.out.println("Мили " + miles);
-                        System.out.println("Ярды " + yards);
-                        System.out.println("Футы " + futs);
                 }
                 break;
+            case 2:
+                 System.out.println("Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут");
+                 int value = console.nextInt();
+                 double meters;
+                 double miles;
+                 double yards;
+                 double futs;
+                 System.out.println("Введите число:");
+                 int count = console.nextInt();
+                        switch (value) {
+                            case 1:
+                                meters = count * 1;
+                                miles = count / 1609.34;
+                                yards = count * 1.094;
+                                futs = count * 3.281;
+                                System.out.println("Метры " + meters);
+                                System.out.println("Мили " + miles);
+                                System.out.println("Ярды " + yards);
+                                System.out.println("Футы " + futs);
+                                break;
+                            case 2:
+                                meters = count * 1609;
+                                miles = count * 1;
+                                yards = count * 1760;
+                                futs = count * 5280;
+                                System.out.println("Метры " + meters);
+                                System.out.println("Мили " + miles);
+                                System.out.println("Ярды " + yards);
+                                System.out.println("Футы " + futs);
+                                break;
+                            case 3:
+                                meters = count / 1.094;
+                                miles = count / 1760;
+                                yards = count * 1;
+                                futs = count * 3;
+                                System.out.println("Метры " + meters);
+                                System.out.println("Мили " + miles);
+                                System.out.println("Ярды " + yards);
+                                System.out.println("Футы " + futs);
+                                break;
+                            case 4:
+                                meters = count / 3.281;
+                                miles = count / 5280;
+                                yards = count / 3;
+                                futs = count * 1;
+                                System.out.println("Метры " + meters);
+                                System.out.println("Мили " + miles);
+                                System.out.println("Ярды " + yards);
+                                System.out.println("Футы " + futs);
+                        }
+                        break;
+                }
         }
     }
-}
+
--- src/TaskTen.java
+++ src/TaskTen.java
@@ -9,12 +9,13 @@ public class TaskTen {
         System.out.print("Введите количество столбцов массива: ");
         int b = console.nextInt();
         int[][] matrix = new int[a][b];
-        System.out.println("Введите элементы строк: ");
+        System.out.println("Введите элементы массива: ");
         for (int j = 0; j < matrix.length; j++) {
-            matrix[0][j] = console.nextInt();
-            matrix[0][j] *= 3;
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][j] = console.nextInt();
+                matrix[0][j] *= 3;
+            }
         }
-
         Arrays.stream(matrix).map(Arrays::toString).forEach(System.out::println);
     }
 }
